41b263d786708b6e185659887136cfbaa44dd0e1
lagrasanomuere <bnett106@gmail.com>
2026-07-05 13:22:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const video = document.getElementById("video");
-
-const videoSrc = "http://127.0.0.1:8888/live/mistream/index.m3u8";
+const url = "http://127.0.0.1:8888/live/mistream/index.m3u8";
 
 if (Hls.isSupported()) {
-    const hls = new Hls();
-    hls.loadSource(videoSrc);
+    const hls = new Hls({
+        lowLatencyMode: true
+    });
+
+    hls.loadSource(url);
     hls.attachMedia(video);
+
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        video.play();
+    });
+
 } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-    video.src = videoSrc;
+    video.src = url;
+    video.play();
 }
