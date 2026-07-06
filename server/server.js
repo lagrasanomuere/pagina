847af00bebf69b5d887431cce26593dce9e32cc2
lagrasanomuere <bnett106@gmail.com>
2026-07-05 21:35:23 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,19 @@
 const express = require("express");
-const getKickData = require("./kick");
+const path = require("path");
 const getTwitchData = require("./twitch");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Servidor funcionando correctamente");
-});
+// Servir la página web
+app.use(express.static(path.join(__dirname, "..")));
 
 app.get("/api/viewers", async (req, res) => {
 
-    const kick = await getKickData();
     const twitch = await getTwitchData();
 
     res.json({
-        kick: kick.viewers,
-        twitch: twitch.viewers,
-        total: kick.viewers + twitch.viewers,
-        liveKick: kick.live,
-        liveTwitch: twitch.live
+        viewers: twitch.viewers,
+        live: twitch.live
     });
 
 });
